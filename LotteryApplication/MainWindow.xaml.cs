dd08b0bd39f0589a689fa2ab99a208db50d55af8
Natlinux81 <natlinux81@vodafone.de>
2022-02-23 01:13:13 +0100

    Initial commit

---
+++ LotteryApplication/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LotteryApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void StartRaffel()
+        {
+            Random rnd = new Random();
+
+
+        }
+
+        private void BtnRaffleClick(object sender, RoutedEventArgs e)
+        {
+            TicketList.Items.Clear();
+            Random rnd = new Random();
+            for(int i = 0; i <= 99; i++)
+            
+            TicketList.Items.Add(rnd.Next(10000));
+        }
+
+        private void BtnExit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void BtnMinimizeClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
